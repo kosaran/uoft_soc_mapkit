9ede8f8adbbe73eb87b6b4190762f6465300104a
kosaran <kosaran.gumarathas@mail.utoronto.ca>
2024-07-23 12:17:21 -0400

    finished

--- mapkit-integration/MapViewViewController.swift
+++ mapkit-integration/MapViewViewController.swift
@@ -9,6 +9,17 @@ import UIKit
 import Foundation
 import MapKit
 
+class AnnotationPin: NSObject, MKAnnotation {
+    var title: String?
+    var subtitle: String?
+    var coordinate: CLLocationCoordinate2D
+    
+    init(title: String, subtitle: String, coordinate: CLLocationCoordinate2D) {
+        self.title = title
+        self.subtitle = subtitle
+        self.coordinate = coordinate
+    }
+}
 class MapViewViewController: UIViewController, MKMapViewDelegate, MapViewModelDelegate {
     
     let mapViewModel = MapViewModel()
@@ -27,9 +38,28 @@ class MapViewViewController: UIViewController, MKMapViewDelegate, MapViewModelDe
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        let highPark = CLLocationCoordinate2D(latitude: 43.6465, longitude: -79.4637)
+        
+        let highParkPin = AnnotationPin(title: "High Park", subtitle: "Cherry Blossom", coordinate: highPark)
+        mapview.addAnnotation(highParkPin)
 
-        // Do any additional setup after loading the view.
+        let rom = CLLocationCoordinate2D(latitude: 43.6677, longitude: -79.3948)
+        let romPin = AnnotationPin(title: "ROM", subtitle: "Museum", coordinate: rom)
+        mapview.addAnnotation(romPin)
+
+        let region = MKCoordinateRegion(center: rom, latitudinalMeters: 1000, longitudinalMeters: 1000)
+        
+        mapview.setRegion(region, animated: true)
+        
+
+        mapview.delegate = self
+        
     }
+    
+
+    
     
     func didUpdateModelState(newState: MapViewModel.LocationModelState) {
         
